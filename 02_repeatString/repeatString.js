ff4b247281225f3f498910f5d4486375a4c2f939
uncle_aitch <hamid_10mohamadi@yahoo.com>
2024-07-16 08:32:26 +0330

    solve repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,repeatCount = 1 ) {
+    if (repeatCount < 0) return "ERROR";
+    let ans = ""
+    while (repeatCount){
+        ans += string;
+        repeatCount -= 1;
+    }
+    return ans;
 };
 
 // Do not edit below this line
