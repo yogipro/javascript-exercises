d5097cb27691d4ccc16e4e9ea6d91189d1c9c34c
uncle_aitch <hamid_10mohamadi@yahoo.com>
2024-07-16 10:39:00 +0330

    solve sumAll problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a , b) {
+    if (a == b) return a;
+    if (typeof a != "number" || typeof b != "number") return "ERROR";
+    if (a < 0 || b < 0) return "ERROR"
+    let sum = ((Math.abs(b - a) + 1) * (a + b)) / 2 ;
+    return sum;
 };
 
 // Do not edit below this line
