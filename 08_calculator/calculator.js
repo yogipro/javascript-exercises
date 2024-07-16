3c82700be2bf701114457977d4c2dd090f496f32
uncle_aitch <hamid_10mohamadi@yahoo.com>
2024-07-16 12:21:40 +0330

    solve calculator problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((a,b)=>a+b,0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a,b)=>a*b,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  if (b == 9) return 1;
+	let ans = 1;
+  while (b > 0){
+    ans *= a;
+    b--;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  console.log("a:",a);
+	if (a == 0 || a == 1) return 1;
+  if (a < 0) return "ERROR";
+  return a * factorial( a - 1 );
 };
 
 // Do not edit below this line
