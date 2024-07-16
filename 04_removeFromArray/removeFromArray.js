bcafc1bffe2fbacda6683a6b73bc3da7e6fe80bb
uncle_aitch <hamid_10mohamadi@yahoo.com>
2024-07-16 10:24:29 +0330

    solve removeFromArray problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...toRemove) {
+    const valueIndexMap = getValueIndexMap(array);
+    if (!array) return 
+    if (array.length == 0) return array
+    for (let item of toRemove){
+        let indicies = valueIndexMap[getKey(item)];
+        if (indicies)
+            for (let index of indicies) array[index] = null;
+    }
+    for (let i = 0 ; i < array.length ;){
+        if (array[i] == null) array.splice(i,1);
+        else i++;
+    }
+    return array;
 };
+
+const getValueIndexMap = function(array){
+    const map = {};
+    for (let i = 0 ; i < array.length ; i++){
+        const key = getKey(array[i])
+        if (!map[key]) map[key] = [];
+        map[key].push(i);
+    }
+    return map;
+}
+
+const getKey =  function (value){
+    return value + typeof value;
+} 
 
 // Do not edit below this line
 module.exports = removeFromArray;
