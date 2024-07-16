abd078c5c60f9db5b3dc8619a8e8f7dfeda7916e
uncle_aitch <hamid_10mohamadi@yahoo.com>
2024-07-16 12:59:59 +0330

    solve fibonacci problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n,memo={}) {
+    if (n in memo) return memo[n];
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 1;
+    memo[n] = fibonacci(n - 1 , memo) + fibonacci(n - 2,memo);
+    return memo[n];
 };
 
 // Do not edit below this line
