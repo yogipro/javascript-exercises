534947859e179aea255c1428d214bfc90d69ffc5
uncle_aitch <hamid_10mohamadi@yahoo.com>
2024-07-16 13:19:48 +0330

    solve findTheOlderst problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(data) {
+    return data.reduce((a,b)=>{
+        if (getAge(a)> getAge(b)) {
+            return a;
+        } else {
+            return b;  
+        }
+    },data[0]);
 };
+
+const getAge = function(person){
+    return (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
