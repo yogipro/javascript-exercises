b3cb5d67ab3ae42effddb9802efffaac46a7b589
uncle_aitch <hamid_10mohamadi@yahoo.com>
2024-07-16 12:07:14 +0330

    solve  leapYears problem

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year == 0 || year < 0) return "ERROR";
+    if (!year) return "ERROR";
+    if (year % 400 == 0) return true
+    else if (year % 4 == 0 && year % 100 != 0) return true;
+    else return false;
 };
 
 // Do not edit below this line
