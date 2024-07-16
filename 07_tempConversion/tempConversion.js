350ef8f5916589f0e2e480737ae5e043a74a1512
uncle_aitch <hamid_10mohamadi@yahoo.com>
2024-07-16 12:07:32 +0330

    solve tempConversion problem

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return Number(
+    ((f - 32) * 5 / 9).toFixed(1)
+  );
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return Number(
+    ((c * 9/5) + 32).toFixed(1)
+  );
 };
 
 // Do not edit below this line
