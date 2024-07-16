119055364dfd14a8ec4af8a55d336365909bbe0d
uncle_aitch <hamid_10mohamadi@yahoo.com>
2024-07-16 08:36:53 +0330

    solve reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str == "") return "";
+    if (!str) return "Error";
+    let ans = ""
+    for (let i = str.length - 1 ; i >= 0 ; i--){
+        ans += str[i]
+    }
+    return ans;
+    /* 
+        edgy kid's solution
+        they love the bad one liners :)
+        return str.split("").reverse().join("");
+    */
 };
 
 // Do not edit below this line
