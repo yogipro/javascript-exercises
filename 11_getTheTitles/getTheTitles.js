4436d153ea9b5d30cb91b172953f7638da4f11ec
uncle_aitch <hamid_10mohamadi@yahoo.com>
2024-07-16 13:05:13 +0330

    solve getTheTitles problem

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(data) {
+    if (!data) return;
+    const titlesArray = [];
+    for (const entry of data){
+        titlesArray.push(entry?.title);
+    }
+    return titlesArray;
 };
 
 // Do not edit below this line
